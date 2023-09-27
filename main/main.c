6c48502d80ad8e1228e0c357ee283eca86c0c1b2
Artem Majorov <gad37rus@gmail.com>
2023-09-27 20:52:54 +0300

    refactoring

--- main/main.c
+++ main/main.c
@@ -99,40 +99,40 @@ char* urlencode(char* originalText)
 
 // scan parameters
 static esp_ble_scan_params_t ble_scan_params = {
-		.scan_type              = BLE_SCAN_TYPE_ACTIVE,
-		.own_addr_type          = BLE_ADDR_TYPE_PUBLIC,
-		.scan_filter_policy     = BLE_SCAN_FILTER_ALLOW_ALL,
-		.scan_interval          = 0x50,
-		.scan_window            = 0x30
-	};
+    .scan_type              = BLE_SCAN_TYPE_ACTIVE,
+    .own_addr_type          = BLE_ADDR_TYPE_PUBLIC,
+    .scan_filter_policy     = BLE_SCAN_FILTER_ALLOW_ALL,
+    .scan_interval          = 0x50,
+    .scan_window            = 0x30
+};
 
 // GAP callback
 static void esp_gap_cb(esp_gap_ble_cb_event_t event, esp_ble_gap_cb_param_t *param)
 {
     switch (event) {
-		
-		case ESP_GAP_BLE_SCAN_PARAM_SET_COMPLETE_EVT: 
-				
-			printf("ESP_GAP_BLE_SCAN_PARAM_SET_COMPLETE_EVT\n");
-			if(param->scan_param_cmpl.status == ESP_BT_STATUS_SUCCESS) {
-				printf("Scan parameters set, start scanning for 10 seconds\n\n");
-				esp_ble_gap_start_scanning(10);
-			}
-			else printf("Unable to set scan parameters, error code %d\n\n", param->scan_param_cmpl.status);
-			break;
-		
-		case ESP_GAP_BLE_SCAN_START_COMPLETE_EVT:
-			
-			printf("ESP_GAP_BLE_SCAN_START_COMPLETE_EVT\n");
-			if(param->scan_start_cmpl.status == ESP_BT_STATUS_SUCCESS) {
-				printf("Scan started\n\n");
-			}
-			else printf("Unable to start scan process, error code %d\n\n", param->scan_start_cmpl.status);
-			break;
-		
-		case ESP_GAP_BLE_SCAN_RESULT_EVT:
-			if(param->scan_rst.search_evt == ESP_GAP_SEARCH_INQ_RES_EVT) {
-				if(param->scan_rst.ble_adv[4] == 0xA4 && param->scan_rst.ble_adv[5] == 0xC1 && param->scan_rst.ble_adv[6] == 0x38) {
+        
+        case ESP_GAP_BLE_SCAN_PARAM_SET_COMPLETE_EVT:
+            
+            printf("ESP_GAP_BLE_SCAN_PARAM_SET_COMPLETE_EVT\n");
+            if(param->scan_param_cmpl.status == ESP_BT_STATUS_SUCCESS) {
+                printf("Scan parameters set, start scanning for 10 seconds\n\n");
+                esp_ble_gap_start_scanning(10);
+            }
+            else printf("Unable to set scan parameters, error code %d\n\n", param->scan_param_cmpl.status);
+            break;
+        
+        case ESP_GAP_BLE_SCAN_START_COMPLETE_EVT:
+            
+            printf("ESP_GAP_BLE_SCAN_START_COMPLETE_EVT\n");
+            if(param->scan_start_cmpl.status == ESP_BT_STATUS_SUCCESS) {
+                printf("Scan started\n\n");
+            }
+            else printf("Unable to start scan process, error code %d\n\n", param->scan_start_cmpl.status);
+            break;
+        
+        case ESP_GAP_BLE_SCAN_RESULT_EVT:
+            if(param->scan_rst.search_evt == ESP_GAP_SEARCH_INQ_RES_EVT) {
+                if(param->scan_rst.ble_adv[4] == 0xA4 && param->scan_rst.ble_adv[5] == 0xC1 && param->scan_rst.ble_adv[6] == 0x38) {
                     bt_failed = false;
                     bool add = true;
                     uint8_t mac[6];
@@ -204,18 +204,14 @@ static void esp_gap_cb(esp_gap_ble_cb_event_t event, esp_ble_gap_cb_param_t *par
                         device_list[i].bat_v = bat_v;
                     }
                 }
-			} else if(param->scan_rst.search_evt == ESP_GAP_SEARCH_INQ_CMPL_EVT){
-                
-				printf("Scan complete\n\n");
-				
-			}
-			break;
-		
-		default:
-		
-			printf("Event %d unhandled\n\n", event);
-			break;
-	}
+            } else if(param->scan_rst.search_evt == ESP_GAP_SEARCH_INQ_CMPL_EVT){
+                printf("Scan complete\n\n");
+            }
+            break;
+        default:
+            printf("Event %d unhandled\n\n", event);
+            break;
+    }
 }
 
 //END BT CODE
@@ -362,7 +358,11 @@ static esp_err_t telegram_post_handler(httpd_req_t *req)
                 url = malloc(800);
                 char *device;
                 
-                sprintf(url, "http://%s/bot%s/sendMessage?chat_id=%d&text=", TELEGRAM_HTTP_PROXY_SERVER, TELEGRAM_TOKEN, cjson_content_message_chat_id->valueint);
+                sprintf(url, "http://%s/bot%s/sendMessage?chat_id=%d&text=",
+                    TELEGRAM_HTTP_PROXY_SERVER,
+                    TELEGRAM_TOKEN,
+                    cjson_content_message_chat_id->valueint
+                );
                 
                 int i;
                 if (full_report == true) {
@@ -433,7 +433,6 @@ static esp_err_t telegram_post_handler(httpd_req_t *req)
     } else {
         free(content);
     }
-    
     return ESP_OK;
 }
 
@@ -448,7 +447,6 @@ static const httpd_uri_t telegram = {
     .method    = HTTP_POST,
     .handler   = telegram_post_handler
 };
-
 
 static httpd_handle_t start_webserver(void)
 {
@@ -478,8 +476,7 @@ static void stop_webserver(httpd_handle_t server)
     httpd_stop(server);
 }
 
-static void disconnect_handler(void* arg, esp_event_base_t event_base,
-                               int32_t event_id, void* event_data)
+static void disconnect_handler(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data)
 {
     httpd_handle_t* server = (httpd_handle_t*) arg;
     if (*server) {
@@ -488,8 +485,7 @@ static void disconnect_handler(void* arg, esp_event_base_t event_base,
     }
 }
 
-static void connect_handler(void* arg, esp_event_base_t event_base,
-                            int32_t event_id, void* event_data)
+static void connect_handler(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data)
 {
     httpd_handle_t* server = (httpd_handle_t*) arg;
     if (*server == NULL) {
@@ -527,35 +523,35 @@ void app_main(void)
     //START BT CODE
     
     printf("BT scan\n\n");
-	
-	// set components to log only errors
-	esp_log_level_set("*", ESP_LOG_ERROR);
-	
-	// initialize nvs
-	ESP_ERROR_CHECK(nvs_flash_init());
-	printf("- NVS init ok\n");
-	
-	// release memory reserved for classic BT (not used)
-	ESP_ERROR_CHECK(esp_bt_controller_mem_release(ESP_BT_MODE_CLASSIC_BT));
-	printf("- Memory for classic BT released\n");
-	
-	// initialize the BT controller with the default config
-	esp_bt_controller_config_t bt_cfg = BT_CONTROLLER_INIT_CONFIG_DEFAULT();
+    
+    // set components to log only errors
+    esp_log_level_set("*", ESP_LOG_ERROR);
+    
+    // initialize nvs
+    ESP_ERROR_CHECK(nvs_flash_init());
+    printf("- NVS init ok\n");
+    
+    // release memory reserved for classic BT (not used)
+    ESP_ERROR_CHECK(esp_bt_controller_mem_release(ESP_BT_MODE_CLASSIC_BT));
+    printf("- Memory for classic BT released\n");
+    
+    // initialize the BT controller with the default config
+    esp_bt_controller_config_t bt_cfg = BT_CONTROLLER_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK(esp_bt_controller_init(&bt_cfg));
-	printf("- BT controller init ok\n");
-	
-	// enable the BT controller in BLE mode
+    printf("- BT controller init ok\n");
+    
+    // enable the BT controller in BLE mode
     ESP_ERROR_CHECK(esp_bt_controller_enable(ESP_BT_MODE_BLE));
-	printf("- BT controller enabled in BLE mode\n");
-	
-	// initialize Bluedroid library
-	ESP_ERROR_CHECK(esp_bluedroid_init());
+    printf("- BT controller enabled in BLE mode\n");
+    
+    // initialize Bluedroid library
+    ESP_ERROR_CHECK(esp_bluedroid_init());
     ESP_ERROR_CHECK(esp_bluedroid_enable());
-	printf("- Bluedroid initialized and enabled\n");
-	
-	// register GAP callback function
-	ESP_ERROR_CHECK(esp_ble_gap_register_callback(esp_gap_cb));
-	printf("- GAP callback registered\n\n");
+    printf("- Bluedroid initialized and enabled\n");
+    
+    // register GAP callback function
+    ESP_ERROR_CHECK(esp_ble_gap_register_callback(esp_gap_cb));
+    printf("- GAP callback registered\n\n");
     
     while(true){
         printf("free_heap_size: %d\n", esp_get_free_heap_size());
